506cf2f278c1164a0f72775c9180b6ea93d6fdaf
Taohid0 <taohidulii@gmail.com>
2017-11-07 07:18:11 +0600

    Update 2

---
+++ src/orbit_coaching/Input_marks_configuration.java
@@ -0,0 +1,9 @@
+package orbit_coaching;
+
+import javax.swing.*;
+
+public class Input_marks_configuration {
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JTextField textField1;
+}
