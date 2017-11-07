1b72ce025867ee3ce1cc022b4d50a8bb3ab830ee
Taohid0 <taohidulii@gmail.com>
2017-11-07 06:32:39 +0600

    GUI update 1

---
+++ src/orbit_coaching/Edit_student.java
@@ -0,0 +1,22 @@
+package orbit_coaching;
+
+import javax.swing.*;
+
+public class Edit_student {
+
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField a500TextField;
+    private JTextField textField5;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField10;
+    private JTextField textField12;
+    private JTextField textField13;
+    private JButton SAVEEDITEDDATAButton;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JComboBox comboBox4;
+}
